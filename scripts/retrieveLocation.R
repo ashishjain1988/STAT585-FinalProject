afa32750d7d53d46682a8f545a9a6c357540f16f
Ashish <jain.ashishjain1@gmail.com>
2017-04-23 16:47:02 -0500

    R scripts for getting venue location

---
+++ scripts/retrieveLocation.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+github<-"https://raw.githubusercontent.com/ashishjain1988/STAT585-FinalProject/master/data/"
+matches <- read.csv(paste0(github,"matches.csv"), stringsAsFactors = FALSE)
+cities<-unlist(matches %>% distinct(city) %>% filter(city != ""))
+points<-geocode(cities)
+row.names(points)<-cities
+points<-points[,2:3]
+coord2loc <- function(points)
+{
+  countriesSP <- getMap(resolution='low')
+  pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(countriesSP)))  
+  indices = over(pointsSP, countriesSP)
+  #indices$continent   # returns the continent (6 continent model)
+  #indices$REGION   # returns the continent (7 continent model)
+  indices$ADMIN  #returns country name
+  #indices$ISO3 # returns the ISO3 code 
+}
+countries<-coord2loc(as.matrix(points))
+points[,3]<-countries
+points["Kochi",]$V3<-"India"
+write.csv(points,"location_mapping.csv")
